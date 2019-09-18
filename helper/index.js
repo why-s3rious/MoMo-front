c39de35d39ef6feb81ba2c12b645b5d71bf28e01
ngocthien0507 <48592936+ngocthien0507@users.noreply.github.com>
2019-09-18 23:08:02 +0700

    aksdahsd

--- helper/index.js
+++ helper/index.js
@@ -1,14 +1,29 @@
 import { hostAPI } from '../configs/index';
 import axios from 'axios';
+<<<<<<< Updated upstream
 const localHost = ':80/v1';
 // :80/v1/search?q=cafe&sort=%2B-time%2Fdistance%2Fmatch&p=2&category=1&location=long,lat
 // getToken = () =>{
 let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Njg4NzE1MjksImlhdCI6MTU2ODc4NTEyOSwic3ViIjoxMDAyMzU1MTgxODAyMn0.VKVdA9eUu9mc26-fGhivrL_dYTev1Wk2p1Xoc35uIJw";
 // }
+=======
+import {AsyncStorage} from 'react-native';
+const localHostLogin = 'v1/auth';
+const localHost = 'v1';
+>>>>>>> Stashed changes
+
+let getTokenFromAsyncStorage = async () => {
+    try {
+        return await AsyncStorage.getItem('@Token');
+    } catch (error) {
+        console.log("Error getting Token" + error);
+        return null;
+    }
+}
 
 export const requestStoreApi = async (endpoint, method, searchText, sort, page, categoryId, location) => {
     console.log(`${hostAPI}${localHost}/${endpoint}?q=${searchText}&sort=${sort}&p=${page}&category=${categoryId}&location=${location}`)
-    const header = 'fake user token';
+    const token = getTokenFromAsyncStorage();
     return axios({
         method: method,
         url: `${hostAPI}${localHost}/${endpoint}?q=${searchText}&sort=${sort}&p=${page}&category=${categoryId}&location=${location}`,
